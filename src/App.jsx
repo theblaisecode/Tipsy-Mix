4d8bebe5cbb039751a41c1a4bf799d2a39c07708
theblaisecode <theblaisecode@gmail.com>
2024-08-30 09:05:33 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   About,
   Cocktail,
@@ -23,7 +22,7 @@ const queryClient = new QueryClient({
   },
 });
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   [
     {
       path: "/",
@@ -61,7 +60,6 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
